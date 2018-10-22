341d7cad844e587184afa25a6a4afd2ac95a1025
Jude Lawson <lawsonjdutton@gmail.com>
2018-10-22 14:58:01 -0600

    Sets up database connection with configuration variables

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,21 @@
-const express = require('express');
-const server = express();
-const bodyParser = require('body-parser')
-const port = process.env.PORT || 3000;
+// Server dependencies
+const express     = require('express');
+const server      = express();
+const bodyParser  = require('body-parser')
+const port        = process.env.PORT || 3001;
+const environment =  require('../.env');
+environment.setup();
+
+// Database dependencies and setup
+const mysql      = require('mysql2');
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'jude',
+  password : process.env.DATABASE_PASSWORD,
+  database : 'menagerie'
+});
+
+connection.connect();
 
 server.use(bodyParser());
 server.use(bodyParser.json());
@@ -10,5 +24,16 @@ server.use(bodyParser.urlencoded({ extended: true }));
 server.get('/', (request, response) => { response.status(200).json({ hello: 'BookIt!' }) } )
 
 server.listen(port, () => console.info(`BookIt is listening on port ${port}`))
+
+process.on('SIGTERM', () => { 
+  console.log('\nClosing server...');
+  connection.end();
+  process.exit();
+})
+process.on('SIGINT', () => { 
+  console.log("\nClosing server...");
+  connection.end();
+  process.exit();
+})
 
 
